38611373900d9a557e1c66688a964459988a1dd3
vasil <vdvasilev2003@gmail.com>
2025-09-11 14:15:59 +0300

    now up3 works properly added some rtop stack routins to itsy.asm some macro and wordnames was renamed

--- itsy.asm
+++ itsy.asm
@@ -86,61 +86,80 @@ lbl macro anam
 _&anam:
 ENDM
 
-dat macro anam,adr
+dat macro anam,adr	;define data field
 @_&anam dw adr
 ENDM
 
-xto macro anam,adr
-to_&anam dw adr
+xto macro anam,adr	;define second code field
+to_&anam: dw adr
 ENDM
 
-xat macro anam,adr
-at_&anam dw adr
+xat macro anam,adr	;define third code field
+at_&anam: dw adr
 ENDM
 
-xt macro anam,adr
+xt macro anam,adr	;define code field
   lbl anam
   dw adr
   endm
 
 cnst macro anam,data
-  xt anam,@doconst
-  dat anam,data
+  xt 	anam,@doconst
+  dat 	anam,data
+  endm
+  
+defer macro anam,data
+  xt 	anam,@defer
+  dat 	anam,data
   endm
   
 var macro anam,data
-  xt anam,@dovar
-  dat anam,data
+  xt 	anam,@dovar
+  dat 	anam,data
   endm
   
 value macro anam,data
-  xto anam,@setvar
-  xt  anam,@dovar
-  dat anam,data
+  xto  	anam,@setvar
+  cnst 	anam,data
+  endm
+  
+WATCH macro anam,data
+  xat 	anam,@dovar-2
+  xto 	anam,@setwatch
+  xt  	anam,@getwatch
+  dat 	anam,data
   endm
   
 vector macro anam,data
-  xto anam,@setvar
-  xt anam,@defer
-  dat anam,data
+  xto 	anam,@setvar
+  defer anam,@defer
+  dat 	anam,data
   endm
   
 point  macro anam,data
-  xto anam,@setpoint
-  xt anam,@dopoint
-  dat anam,data
+  xto 	anam,@setpoint
+  xt 	anam,@dopoint
+  dat 	anam,data
   endm
 
-twice3 macro anam
-  xt anam,@twice-2
+trap3 macro anam
+  xt anam,@trap-3
 endm
 
-twice2 macro anam
-  xt anam,@twice-1
+trap2 macro anam
+  xt anam,@trap-2
+endm
+
+trap1 macro anam
+  xt anam,@trap-1
+endm
+
+trap macro anam
+  xt anam,@trap
 endm
 
 twice macro anam
-  xt anam,@twice
+  dw @trap
 endm
 
 prim macro anam
@@ -192,16 +211,18 @@ Start   Label byte
 	mov bl,[bx]
 @lobyte:
 	mov bh,0
-	jmps next
+	jmps @donext
 @dup:
 	push bx
-	jmps next
+	jmps @donext
+@getwatch:
+	mov	di,[di]
 @doconst:
 	mov ax,[di]
 	jmp @ph
 @XA:
     xchg    DX,[BP]                 ;XA
-    jmps  next
+    jmps  @donext
 @@RSST:
 	POP	AX
 	xchg    AX,bx
@@ -212,36 +233,33 @@ Start   Label byte
 	call    DI
 @@EX:
     xchg    SI,[BP]
-    jmps  next
+    jmps  @donext
 
 @wary:
 	shl bx,1
 @bary:
 	add bx,di
-	jmps next
+	jmps @donext
 @str:  
 	lea ax,[bx+2]
 	push ax
-	jmps @fetch
-@fetch2:
-	push [bx+2]
 @fetch:
 	mov bx,[bx]
-	jmps next
+	jmps @donext
 
 @minus:
 		neg bx
 @plus:
         pop ax
         add bx,ax
-        jmps next
+        jmps @donext
 @equals:
         pop ax
         sub bx,ax
 @zeq:		
         sub bx,1
         sbb bx,bx
-        jmps next		
+        jmps @donext		
 		
 @col_eval:
 	pop  ax
@@ -257,7 +275,7 @@ Start   Label byte
 	jmps @pcpush
 @xr:
 	xchg bx,[bp]
-    jmps next
+    jmps @donext
 @j:
 	mov	ax,[bp+2]
 	jmps @ph
@@ -289,10 +307,10 @@ Start   Label byte
         push bx
 @sw:
         xchg ax,bx
-next:   
+@donext:   
 		mov di,[si]
 		cmpsw
-next1:		
+@donext1:		
         jmp [di-2]
 
 ; ^^^^^^^^^^^^^^^^^^
@@ -300,7 +318,8 @@ next1:
 ; -------------------
 
 @SETPOINT:
-	mov [di+2],si
+	scasw
+	mov [di],si
 	jmps @rts
 @0ex:	
 	or bx,bx
@@ -310,7 +329,7 @@ next1:
     pop [bx]
 @drop:		
     pop bx
-    jmps next
+    jmps @donext
 	
 @dropx:
 	pop bx
@@ -320,11 +339,16 @@ next1:
 	inc bp
 	inc bp
 @none:	
-	jmps next
+	jmps @donext
 		
 	scasw
 	scasw
-@twice:
+	scasw
+@trap:
+	skipr cx
+@cont:
+	dec di
+	dec di
 	mov ax,di
 	jmps @rpush
 @execute:
@@ -361,7 +385,7 @@ next1:
 	mov di,bx
 	scasw
     pop bx
-	jmps next1
+	jmps @donext1
 	
 @zero_branch:
 	test bx,bx
@@ -369,7 +393,7 @@ next1:
 	je @br
 @skip:		
 	lodsw
-	jmps next
+	jmps @donext
 @mif:
 		inc bx
 @ifm:	
@@ -377,7 +401,7 @@ next1:
 	js @skip
 @br:
 	mov si,[si]
-	jmps next
+	jmps @donext
 @comma@:
 	mov bx,[bx]
 @comma:		
@@ -386,21 +410,29 @@ next1:
 	stosw
 	mov [@_dp],di
 	jmps @drop
+@setwatch:
+	mov di,[di+2]
+	skipb
 @setvar:
-		mov [di+2],bx
-		jmps @drop
+	scasw
+	skipb
+@swapstor:
+	pop DI
+	mov [di],bx
+	jmps @drop
 
 @@BINU_:  PUSH BX
 @@DINU_:  POP   CX
 @@NINU_:  POP   AX
 @@NUP_:   CALL  DI
 		PUSH  AX
-        jmps next2
+@donext2:
+        jmps @donext
 
 @@cnip_:  POP   cX
 @@nip_:   POP   AX
 @@nop_:   CALL  DI
-        jmps next2
+        jmps @donext2
 
 @@DROP3_: POP   CX        ;DRP3
 @@DROP2_: POP   AX        ;DRP2
@@ -430,43 +462,43 @@ next1:
 	sub di,bx
 	push di
 	mov bx,cx
-	jmps next2
+	jmps @donext2
 	
-@to_number: ; di num   ax adrs  cx leng  bx - base
-	pop cx ax di
-	push si
-	xor si,si
-	xchg ax,si
-@@to_numl:
-	jcxz @@to_numz
-	lodsb
-	cmp al,'9'+1
-	jc @@to_numg
-	cmp al,'A'
-	jc @@to_numh
-	sub al,7
-@@to_numg:
-	sub al,48
-    cmp ax,bx
-    jnc @@to_numh
-	xchg ax,di
-	push dx
-	mul bx
-	pop dx
-	xchg ax,di
-	add di,ax
-	dec	cx
-	jmp @@to_numl
-@@to_numh:
-	dec si
-@@to_numz:
-	xchg ax,si
-	pop si
-@@found:		
-	push ax
-	mov  bx,cx
-next2:	
-	jmp next
+;@to_number: ; di num   ax adrs  cx leng  bx - base
+;	pop cx ax di
+;	push si
+;	xor si,si
+;	xchg ax,si
+;@@to_numl:
+;	jcxz @@to_numz
+;	lodsb
+;	cmp al,'9'+1
+;	jc @@to_numg
+;	cmp al,'A'
+;	jc @@to_numh
+;	sub al,7
+;@@to_numg:
+;	sub al,48
+;    cmp ax,bx
+;    jnc @@to_numh
+;	xchg ax,di
+;	push dx
+;	mul bx
+;	pop dx
+;	xchg ax,di
+;	add di,ax
+;	dec	cx
+;	jmp @@to_numl
+;@@to_numh:
+;	dec si
+;@@to_numz:
+;	xchg ax,si
+;	pop si
+;@@found:		
+;	push ax
+;	mov  bx,cx
+; @donext2:	
+;	jmp @donext
 
   XT	MAKESTR,@@cnip_
 	mov [BX],CL                ; SET strlen
@@ -499,22 +531,15 @@ next2:
   XT	CMOVE,@@DROP3_
 	JMPS @@CMOVE
 		
-  xt zswap,@@nup_
-	skipr ax
-	
-  lbl bye		
-	dw 0	;addres of bye
-	ret
-
   xt dk,@@nup_
-getchar:
+@getchar:
 	mov ah,7
 	int 021h
 	mov ah,0
 	ret
 
   xt de,@@drop_
-outchar:
+@outchar:
 	mov ah,2
 @dos:
 	push dx
@@ -529,14 +554,14 @@ outchar:
 
         point found,0
 		point init,_initadr
-        vector errv,0
-        vector key,_dk
-        vector emit,_de
-		vector accept,_accpt
+        defer errv,0
+        defer key,_dk
+        defer emit,_de
+		defer accept,_accpt
         value ltib,0
         value etib,0
         value erra,0
-        value dp,freemem
+        value dp,@freemem
         value base,10
         value dict,0f000h-270
         cnst  tib,080h
@@ -555,7 +580,7 @@ outchar:
 		call @does
 @COMM   dw _str,_comma,_@exec,_exit
 				
-		twice3 xcomma
+		trap2 xcomma
 		xt litc,@commaer
 		dw _lit,_comma,_exit
 		
@@ -567,15 +592,22 @@ outchar:
 ; Stack
 ; -------------------
 
-        xt drop,@drop
-        xt dup,@dup
-        xt swap,@swap
-        xt pop,@pop
-        xt push,@push
-		xt RDROP,@RDROP
-		xt j,@j
-		xt xr,@xr
-		xt xa,@xa
+  xt drop,@drop
+  xt dup,@dup
+  xt swap,@swap
+  xt pop,@pop
+  xt push,@push
+  xt RDROP,@RDROP
+  xt j,@j
+  xt xr,@xr
+  xt xa,@xa
+		
+  xt zswap,@@nup_
+	skipr ax	
+  lbl bye		
+	dw 0	;addres of bye
+	ret
+
 
 ; -------------------
 ; Maths / Logic
@@ -590,7 +622,7 @@ outchar:
     AND   BX,AX
     RET
 
-  xt XORW,@@nip_
+  xt XOR,@@nip_
     XOR   BX,AX
     RET
 
@@ -627,6 +659,7 @@ outchar:
   xt store ,@store
   xt C@ ,@C@
   xt str,@str
+  xt swapstor,@swapstor
 
   xt stm,@@nip_
 	mov	[bx-2],ax
@@ -666,12 +699,11 @@ outchar:
   xt semicolon,@commaer
 	dw _exit,_lpar
   
-  twice3 xfind
   col lpar
 	dw to_found,_dict,_find,_exit
   
   col rpar
-	dw to_found,_dict,_skpdtok,_findc,_exit
+	dw to_found,_dict,_findc,_exit
 		
 ; -------------------
 ; Flow Control
@@ -691,32 +723,62 @@ outchar:
   xt dropx,@dropx
   xt 0x,@0ex
 
-  col TIMES
-	dw _push,_xr
-  col xTIMES
-	dw _bran,@@tim2
-@@tim1 dw _push,_j,_execute,_pop
-@@tim2 dw _ifm,@@tim1,_rdrop,_dropx	
+;  col TIMES
+;	dw _push,_xr
+;  col xTIMES
+;	dw _bran,@@tim2
+;@@tim1 dw _push,_j,_execute,_pop
+;@@tim2 dw _ifm,@@tim1,_rdrop,_dropx	
   
 ; -------------------
 ; String
 ; -------------------
 
+  XT  to_number,@@Binu_
+	push 	SI
+	xchg 	ax,SI  	; START ADR
+	mov		BL,10	; NUM BASE
+	XOR		ax,ax
+	XOR		DI,DI	; ZERO ACCUM
+	jmps @@NUM
+	
+@@Nm:xchg 	ax,DI
+    mul  	BX
+	xchg 	ax,DI
+    ADD		DI,AX
+	DEC      cx
+@@NUM:   JZ @@EXNUM
+	lodsb
+	CMP		AL,'$'
+	JNE	@@NM2
+	mov		BL,16
+	JMPS	@@NUM
+@@NM2:
+	cmp  	al,'9'+1
+	jc   @@n
+	cmp  al,'A'
+	jc   @@EXNUM
+	sub  al,7
+@@n: sub  al,'0'
+	cmp  ax,BX
+	jc   @@NM
+@@EXNUM: 
+	POP 	SI
+	XCHG	DI,AX	; RESULT NUMBER IN AX
+	mov		BX,CX
+	RET
+
   xt count,@count
-  xt to_number,@to_number
+;  xt to_number,@to_number
 
   XT STRP,@@niNU_
     INC   ax
     DEC   BX
 @TROFF:
     ret
-
 	
   col to_num
-	dw _xdrop,_zswap,_count,_base,_to_number
-	
-  col error?
-	dw _strskp,to_erra,_0x,_errv,_abort,_err?,_exit
+	dw _xdrop,_zswap,_count,_base,_to_number,_err?,_exit
 	
   col NUMC
 	dw _to_num,_litc,_exit
@@ -724,7 +786,11 @@ outchar:
 ; -----------------------
 ; errors prompt
 ; -----------------------
-  twice xnz?
+
+  col error?
+	dw _strskp,to_erra,_0x,_errv,_abort
+	
+  trap xnz?
 	dw _zeq
   col err?
     dw _error?
@@ -745,47 +811,59 @@ outchar:
   col parse
 	dw _etib,_ltib,_pars,to_ltib,_exit
 	
-  twice xtostr
+  trap xtostr
 	dw _tbuf,_makestr,_exit
 	
-  twice xdc@
+  trap xdc@
 	dw _dup,_c@,_exit
 
 ; -----------------------
 ; Dictionary Search
 ; -----------------------
 
+@dofindc:
+	mov  cx,3
+	add  cl,[bx+2]
+	jmps @dofind2
+
   xt find,@@ninu_
-	mov di,ax
+	xchg si,ax
 @dofind:
-    push si di dx
-	mov  di,bx	;ax := di  di := bx
 	xor  cx,cx
+@dofind2:
+    push ax 
+	mov  di,bx	;ax := di  di := bx
 @@findm:
 	add di,cx
-	mov dx,di
+	mov ax,di
 	scasw
 	mov cl,[di]
+	push si
 	jcxz @@findi
 	inc cx
-	mov	si,ax
 	rep cmpsb
+	pop  si
     jne @@findm
-	dec cx
-	xchg ax,dx
-@@findi:
-	pop dx di si
 	inc cx
+	push ax
+@@findi:
+	pop di
+	pop si
 	mov bx,cx
 	ret
 		
   xt findc,@@ninu_
-	mov di,ax
+	xchg si,ax
+	inc  byte ptr [si]
+	push bx
+	call @dofindc
+	pop  cx
 	inc  byte ptr [di]
-	call @dofind
-	inc  byte ptr [di]
-	jz   @@fend
-	call @dofind
+	or  bx,bx
+	jnz   @@fend
+	mov  bx,cx
+	xchg di,ax
+	call @dofindc
 	inc bx
 	inc bx
 @@fend:
@@ -821,15 +899,12 @@ outchar:
 @eval	dw _token,_zbran,@xeval
 		dw _found,_oper,_@exec,_BRAN,@eval
 
-  twice xdrop
+  trap xdrop
 @xeval	dw _dropx
 
-  col skpdtok
-	dw _str,_drop,_count,_plus,_exit
-	
-  col forget1
-  dw _xdict,_dup,_fetch,to_dp,_skpdtok,_exit
-  
+;  col forget1
+;  dw _xdict,_dup,_fetch,to_dp,_skpdtok,_exit
+;  
   col phstr
 	dw _count,_strp
   col phmem
@@ -839,22 +914,37 @@ outchar:
 	dw _dict,_ex,to_dict,_exit
 
   lbl initadr
-	dw  _xpect,_lit,128,_phstr,_dp,_str,_phmem
-	dw to_init
-  col _expect
-	dw _xpect,_exit
-	
-  twice xpect
+	dw  _tib,_phstr,_dp,_str,_phmem,_expect,to_init
+  col expect
 	dw _tib,_inl,_accept,_exit
 		
-freemem:
-  dw final-freemem-2
+@freemem:
+  dw @final-@freemem-2
   dw _header
   aname '=:'
   dw 0
   db 0
-final = $
+  
+@final = $
 
   
 MyCseg  ends
         end  Start
+
+
+not used
+
+@BARY			@CURSEG			@DOFIND			@FETCH2			@LOBYTE			@NONE			
+@SW				@_ACCEPT		@_BASE			@_DICT			@_EMIT			@_ERRA			
+@_ERRV			@_ETIB			@_FOUND			@_INIT			@_INL			@_KEY			
+@_LTIB			@_TBUF			@_TIB			GETCHAR			OUTCHAR			TO_ACCEPT		
+TO_BASE			TO_DP			TO_EMIT			TO_ERRV			TO_KEY			_AND			
+_BYE			_CMOVE			_CMOVEU			_COLC			_CONSTANT		_CREATE			
+_CRSTP			_DROP			_EMIT			_EQUALS			_ERRA			_EXEC			
+_EXECUTE		_FETCH			_FOR			_IFM			_J				_KEY			
+_MIDIV			_MIF			_MINUS			_PARSE			_PL2DIV			_PLMUL			
+_PLUS			_POP			_PUSH			_RDROP			_RLDP			_RSTP			
+_SEMICOLON		_SKIP			_STORE			_SWAP			_XA				_XCOMMA			
+_XOR			_XR				__EXPECT		COLON			CONSTANT		HEADER			
+PRIM			PRIMITIVE		trap1			VAR				VARIABLE		XAT				
+XTNAME			
